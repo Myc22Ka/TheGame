190fd89bf79e82e900ef541e26bdbf3c0e5ce4bb
Firecry <lubiexd80@gmail.com>
2023-07-26 16:32:04 +0200

    game over screen

--- src/layout/GameOver.tsx
+++ src/layout/GameOver.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "../styles/gameover.scss";
+// import image from "../assets/money.webP";
 
 const GameOver: React.FC = () => {
-  return <div className="gameover-container">GameOver</div>;
+  return (
+    <div className="gameover-background">
+      <div className="gameover-container">
+        <div className="gameover-title">Game Over, Time&apos;s Up</div>
+        <div>Your earnings: {/*<img src={image} />*/}$2137</div>
+        <div className="gameover-button">Restart?</div>
+      </div>
+    </div>
+  );
 };
 
 export default GameOver;
