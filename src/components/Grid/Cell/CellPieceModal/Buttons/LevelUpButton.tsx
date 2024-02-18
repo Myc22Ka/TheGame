eadffe173ca8a9aea5bd48f219672f1e915fa320
LonlehKitteh <krzysztof02gach@gmail.com>
2024-02-18 23:03:29 +0100

    buttons styles

--- src/components/Grid/Cell/CellPieceModal/Buttons/LevelUpButton.tsx
+++ src/components/Grid/Cell/CellPieceModal/Buttons/LevelUpButton.tsx
@@ -1,6 +1,6 @@
 import { faCircleUp } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useRef, useState } from "react";
+import React, { useCallback, useState } from "react";
 import { Button, Stack } from "react-bootstrap";
 import { useGame } from "src/contexts/GameContext";
 import { useScore } from "src/contexts/ScoreContext";
@@ -9,83 +9,57 @@ import styles from "src/styles/style.module.scss";
 import { useCell } from "src/contexts/CellContext";
 import { motion } from "framer-motion";
 
-const iconVariants = {
-  inactive: { y: 0, x: 0 },
-  active: { y: [0, 1, -2.69, 0] },
-};
 const buttonVariants = {
-  reject: { scale: 1, x: [0, -5, 5, -2.5, 2.5, 0], rotate: [0, -10, 10, -5, 5, 0] },
-  success: { scale: 1.05, x: [0, 0] },
+  reject: { scale: 1, x: [0, -2.5, 2.5, -1.25, 1.25, 0], rotate: [0, -2.5, 2.5, -1.25, 1.25, 0] },
+  success: { scale: [1, 1.1, 1], x: [0, 0] },
 };
 
 type LevelButtonPropsType = {
   handleClose: () => void;
 };
 
-type ButtonAnimateStates = "inactive" | "active";
-
 const LevelUpButton: React.FC<LevelButtonPropsType> = ({ handleClose }) => {
-  const { score, removeSomeGold, updateActivators, currentGameSpeed } = useScore();
-  const { levelUp } = useGame();
   const { cell } = useCell();
-  const [animateState, setAnimateState] = useState<ButtonAnimateStates>("inactive");
   const { upgradeCost, level, activators } = cell.insideCell;
 
-  const handleLevelUp = () => {
+  if (level === upgradeCost.length) return;
+
+  const { score, removeSomeGold, updateActivators, currentGameSpeed } = useScore();
+  const { levelUp } = useGame();
+  const [isHover, setIsHover] = useState(false);
+
+  const handleLevelUp = useCallback(() => {
     if (
-      score.gold <= upgradeCost[level] ||
+      score.gold < upgradeCost[level] ||
       cell.isDestroyed ||
-      (activators.power && activators.power[level] + score.gameStats.power <= 0) ||
-      !upgradeCost[level]
-    ) {
+      (activators.power && activators.power[level] + score.gameStats.power <= 0)
+    )
       return;
-    }
+
     handleClose();
 
     removeSomeGold(upgradeCost[level]);
     updateActivators(cell.insideCell, "-");
     levelUp(cell);
     updateActivators({ ...cell.insideCell, level: level + 1 });
-  };
+  }, [score.gold, cell.isDestroyed, activators.power, score.gameStats.power]);
 
-  const handleHoverStart = () => setAnimateState("active");
-  const handleHoverEnd = () => setAnimateState("inactive");
   return (
     <Stack direction="horizontal" style={{ backgroundColor: styles.toExpensive, borderRadius: "0.375rem" }}>
-      {level !== upgradeCost.length && (
-        <PieceCostMoney
-          disabled={level === upgradeCost.length || upgradeCost[level - 1] > score.gold || cell.isDestroyed}
-        />
-      )}
+      <PieceCostMoney disabled={upgradeCost[level - 1] > score.gold || cell.isDestroyed} />
       <motion.div
         whileTap={cell.isDestroyed ? buttonVariants.reject : buttonVariants.success}
-        onHoverStart={handleHoverStart}
-        onHoverEnd={handleHoverEnd}
+        onHoverStart={() => setIsHover(true)}
+        onHoverEnd={() => setIsHover(false)}
         transition={{
           duration: currentGameSpeed({ devider: 6000 }),
           ease: "easeInOut",
         }}
       >
-        <Button variant="main" onClick={handleLevelUp}>
+        <Button variant={`main${!cell.isDestroyed ? "-active" : ""}`} onClick={handleLevelUp}>
           <Stack gap={3} direction="horizontal">
-            {!cell.isDestroyed ? (
-              <motion.div
-                animate={animateState}
-                transition={{
-                  times: [0, 0.5, 1],
-                  duration: currentGameSpeed({ devider: 1500 }),
-                  repeat: animateState === "active" ? Infinity : 0,
-                  ease: "easeInOut",
-                }}
-                initial="inactive"
-                variants={iconVariants}
-              >
-                <FontAwesomeIcon icon={faCircleUp} />
-              </motion.div>
-            ) : (
-              <FontAwesomeIcon icon={faCircleUp} />
-            )}
-            <span>{level === upgradeCost.length ? "Max Level" : "Level Up"}</span>
+            <FontAwesomeIcon icon={faCircleUp} bounce={isHover && !cell.isDestroyed} />
+            <span>Level Up</span>
           </Stack>
         </Button>
       </motion.div>
--- src/components/Grid/Cell/CellPieceModal/Buttons/RepairButton.tsx
+++ src/components/Grid/Cell/CellPieceModal/Buttons/RepairButton.tsx
@@ -7,6 +7,12 @@ import { useGame } from "src/contexts/GameContext";
 import { useScore } from "src/contexts/ScoreContext";
 import styles from "src/styles/style.module.scss";
 import PieceCostPower from "./PieceCostPower";
+import { motion } from "framer-motion";
+
+const buttonVariants = {
+  reject: { scale: 1, x: [0, -2.5, 2.5, -1.25, 1.25, 0], rotate: [0, -2.5, 2.5, -1.25, 1.25, 0] },
+  success: { scale: [1, 1.1, 1], x: [0, 0] },
+};
 
 type RepairButtonPropsType = {
   handleClose: () => void;
@@ -14,7 +20,7 @@ type RepairButtonPropsType = {
 
 const RepairButton: React.FC<RepairButtonPropsType> = ({ handleClose }) => {
   const { repairPiece } = useGame();
-  const { score, updateActivators } = useScore();
+  const { score, updateActivators, currentGameSpeed } = useScore();
   const { cell } = useCell();
 
   const handleRepair = () => {
@@ -28,12 +34,20 @@ const RepairButton: React.FC<RepairButtonPropsType> = ({ handleClose }) => {
 
   return (
     <Stack direction="horizontal" style={{ backgroundColor: styles.toExpensive, borderRadius: "0.375rem" }}>
-      <Button variant="main" onClick={handleRepair}>
-        <Stack gap={3} direction="horizontal">
-          <FontAwesomeIcon icon={faWrench} />
-          <span>Repair</span>
-        </Stack>
-      </Button>
+      <motion.div
+        whileTap={!cell.isDestroyed ? buttonVariants.reject : buttonVariants.success}
+        transition={{
+          duration: currentGameSpeed({ devider: 6000 }),
+          ease: "easeInOut",
+        }}
+      >
+        <Button variant={`main${cell.isDestroyed ? "-active" : ""}`} onClick={handleRepair}>
+          <Stack gap={3} direction="horizontal">
+            <FontAwesomeIcon icon={faWrench} shake={cell.isDestroyed} />
+            <span>Repair</span>
+          </Stack>
+        </Button>
+      </motion.div>
       <PieceCostPower disabled={!cell.isDestroyed || score.gameStats.power <= 0} />
     </Stack>
   );
